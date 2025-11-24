323a1bcedbdd1d6f4196c007cf2c3b80d9f7543f
uno/hurtki <asboba2101@gmail.com>
2025-11-24 22:20:27 +0200

    add ( db max connections setting using postgres max connections count + logging it all )

--- internal/repo/tasks/db_init.go
+++ internal/repo/tasks/db_init.go
@@ -16,8 +16,16 @@ type TaskStorage struct {
 }
 
 const (
-	DATABASE_CONNECTION_TRIES_COUNT = 10
-	RETRY_TIME_BETWEEN_TRIES        = 1 * time.Second
+	// DB_MAX_CONNECTIONS_PERCENT_TO_POSTGRES_MAX - constant is a percent of db connections
+	// from postgres max connections
+	dbMaxConnectionsPercentToPostgresMax = 70
+	// DB_MAX_IDLE_CONNECTIONS_PERCENT_TO_POSTGRES_MAX - constant is a percent of db idling connections
+	// from postgres max connections
+	dbMaxIdleConnectionsPercentToPostgresMax = 10
+
+	connectionLifiTime           = 5 * time.Minute
+	dataBaseConnectionTriesCount = 10
+	retryTimeBetweenTries        = 1 * time.Second
 )
 
 func GetTaskStorage(logger slog.Logger) (TaskStorage, error) {
@@ -32,19 +40,19 @@ func GetTaskStorage(logger slog.Logger) (TaskStorage, error) {
 	var db *sql.DB
 	var err error
 
-	for i := range DATABASE_CONNECTION_TRIES_COUNT {
+	for i := range dataBaseConnectionTriesCount {
 		// Connection with data base
 		db, err = sql.Open("pgx", dsn)
 
 		if err != nil {
-			logger.Warn(fmt.Sprintf("Cannot open database(retrying in 1 second), try number: %d/%d", i+1, DATABASE_CONNECTION_TRIES_COUNT), "source", fn)
-			time.Sleep(RETRY_TIME_BETWEEN_TRIES)
+			logger.Warn(fmt.Sprintf("Cannot open database(retrying in 1 second), try number: %d/%d", i+1, dataBaseConnectionTriesCount), "source", fn)
+			time.Sleep(retryTimeBetweenTries)
 			continue
 		}
 
 		if err := db.Ping(); err != nil {
-			logger.Warn(fmt.Sprintf("Cannot ping database, try number: %d/%d", i+1, DATABASE_CONNECTION_TRIES_COUNT), "source", fn)
-			time.Sleep(RETRY_TIME_BETWEEN_TRIES)
+			logger.Warn(fmt.Sprintf("Cannot ping database, try number: %d/%d", i+1, dataBaseConnectionTriesCount), "source", fn)
+			time.Sleep(retryTimeBetweenTries)
 			continue
 		}
 		logger.Info("esablished connection with database")
@@ -52,8 +60,31 @@ func GetTaskStorage(logger slog.Logger) (TaskStorage, error) {
 	}
 
 	if err != nil {
+		logger.Error("Can't establish connection with database", "source", fn, "err", err)
 		return TaskStorage{}, fmt.Errorf("%s:%w", fn, err)
 	}
+
+	maxConnectionsRow := db.QueryRow(`
+	SHOW max_connections;
+	`)
+	var maxPostgresConnections int
+	if err := maxConnectionsRow.Scan(&maxPostgresConnections); err != nil {
+		logger.Error("cannot get postgres max connections", "err", err, "source", fn)
+		return TaskStorage{}, fmt.Errorf("can't get postgres max connections, can't intialize storage: %w", err)
+	}
+
+	logger.Info("got postgres max connections count", "count", maxPostgresConnections, "source", fn)
+
+	var dbMaxOpenCons int = maxPostgresConnections * dbMaxConnectionsPercentToPostgresMax / 100
+	var dbMaxIdleCons int = maxPostgresConnections * dbMaxIdleConnectionsPercentToPostgresMax / 100
+
+	logger.Info("setting db max open connections", "count", dbMaxOpenCons, "source", fn)
+	logger.Info("setting db max idle connections", "count", dbMaxIdleCons, "source", fn)
+
+	db.SetMaxOpenConns(dbMaxOpenCons)
+	db.SetMaxIdleConns(dbMaxIdleCons)
+	logger.Info("setting connection life time", "count", connectionLifiTime, "source", fn)
+	db.SetConnMaxLifetime(connectionLifiTime)
 
 	storage := TaskStorage{
 		logger: logger.With("service", "DataBase"),
--- main.go
+++ main.go
@@ -4,27 +4,26 @@ import (
 	"os"
 
 	tasksHandler "github.com/hurtki/crud/internal/app/tasks"
-	//"github.com/hurtki/crud/internal/config"
-	"github.com/hurtki/crud/internal/domain/tasks"
-	"github.com/hurtki/crud/internal/repo/tasks"
-
-	"github.com/hurtki/crud/internal/logger"
-
 	"github.com/hurtki/routego"
 	"github.com/hurtki/routego/routeSet"
 
+	//"github.com/hurtki/crud/internal/config"
+	"github.com/hurtki/crud/internal/domain/tasks"
+	tasks_repo "github.com/hurtki/crud/internal/repo/tasks"
+
+	"github.com/hurtki/crud/internal/logger"
 )
 
 func main() {
 	logger := logger.NewLogger()
-	// TODO: use app config 
+	// TODO: use app config
 	// config := config.NewAppConfig(":8000")
 	logger.Info("logger initialized")
 
 	storage, err := tasks_repo.GetTaskStorage(*logger)
 
 	if err != nil {
-		logger.Error(err.Error())
+		logger.Info("Can't initialize storage, exiting")
 		os.Exit(0)
 	}
 
@@ -35,7 +34,7 @@ func main() {
 	tasksHandler := tasksHandler.NewTasksHandler(*logger, tasksUseCases)
 	routeSet.Add("/tasks/{num}", tasksHandler.ServeReadUpdateDelete)
 	routeSet.Add("/tasks/", tasksHandler.ServeCreateList)
-	
+
 	routegoConfig := routego.NewRoutegoConfig(":8000")
 	router := routego.NewRouter(*logger, routegoConfig, routeSet)
 	if err := router.StartRouting(); err != nil {
